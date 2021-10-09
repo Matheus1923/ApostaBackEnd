07ebd5140379fcc04c533df299dc52e5cea32ea3
Matheus1923 <theotomi@hotmail.com>
2021-10-09 09:06:13 -0400

    Entidades Clube

--- ApostaBackEnd/ApostaBackEnd/Models/Clube.cs
+++ ApostaBackEnd/ApostaBackEnd/Models/Clube.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ApostaBackEnd.Models
 {
+    [Table("Clube")]
     public class Clube
     {
+        [Key]
+        [Column("idclube")]
+        [Required(ErrorMessage = "O campo deve ser obrigatório")]
+
+        public Int32 IdClube { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "O valor de caracteres deve ser de 10 á 50")]
+        [Column("nome")]
+
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "O valor de caracteres deve ser de 10 á 50")]
+        [Column("telefone")]
+
+        public string Telefone { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "O valor de caracteres deve ser de 10 á 50")]
+        [Column("email")]
+
+        public string Email { get; set; }
     }
 }
