bf24c2587ff221715217815288b9180b4a1ed837
Matheus1923 <theotomi@hotmail.com>
2021-10-09 13:52:54 -0400

    Controller criada

---
+++ ApostaBackEnd/ApostaBackEnd/Models/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApostaBackEnd.Models
+{
+    public class Contexto:DbContext
+    {
+        public DbSet<Aposta> Apostas { get; set; }
+
+        public DbSet<Cliente> clientes { get; set; }
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+
+        }
+    }
+}
